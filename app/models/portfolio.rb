2bb47bc95b379f1b312d7b9ea3b52461028007a6
slcwestdevcamp <slcwest@devcamp.com>
2017-06-21 17:40:47 +0000

    Integrated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,13 @@
 class Portfolio < ApplicationRecord
     has_many :technologies
+    accepts_nested_attributes_for :technologies,
+                                  reject_if: lambda { |attrs| attrs['name'].blank? }
+    
     include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
     
     def self.angular
-        where(subtitle: 'Anuglar')
+        where(subtitle: 'Angular')
     end
     
     scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on Rails')}
