74ed2361db8f1ebca4925e57f2ce3d434f0c8dcc
slcwestdevcamp <slcwest@devcamp.com>
2017-06-21 17:10:00 +0000

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -66,3 +66,11 @@ end
 end
 
 puts "9 potfolio items created"
+
+3.times do |technology|
+   Portfolio.last.technologies.create!(
+       name: "Technology #{technology}",
+       ) 
+end
+
+puts "3 technologies created"
